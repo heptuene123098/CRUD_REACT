4969b4cc773d97be95514e29f8c83f8fb790152f
heptuene123098 <heptuene@gmail.com>
2024-11-26 09:04:45 +0100

    changes

--- react_crud/src/App.js
+++ react_crud/src/App.js
@@ -1,10 +1,30 @@
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
+import ListUser from './components/ListUser';
+import CreateUser from './components/CreateUser';
+
 
 function App() {
   return (
     <div className="App">
-      HELLO REACT
+      <h5>REACT CRUD operations with PHP API and MYSQL</h5>
+
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">List Users</Link>
+            </li>
+            <li>
+              <Link to="user/create">Create User</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route index element={<ListUser />} />
+          <Route path='user/create' element={<CreateUser />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
